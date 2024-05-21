6a76fba25ea74d5b09ed4aa53fa085f85926dc12
domenick-z <vzisimos@csd.auth.gr>
2024-05-21 22:44:16 +0300

    added empty activity for menu

--- app/src/main/java/com/example/cafepda/MenuActivity.java
+++ app/src/main/java/com/example/cafepda/MenuActivity.java
@@ -1,8 +1,6 @@
 package com.example.cafepda;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void TablesActivity(View view){
-        Intent i = new Intent(this, TablesActivity.class);
-
-        startActivity(i);
-
-    }
-
-
 }
